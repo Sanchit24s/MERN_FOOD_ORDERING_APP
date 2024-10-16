149f901774bc86fb39d8ac9615eb60a064a8bda4
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-10-16 19:30:10 +0530

    Auth0 Access Token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,13 +1,13 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
 };
 
 function Auth0ProviderWithNavigate({ children }: Props) {
-    const { createUser } = useCreateMyUser();
+    const navigate = useNavigate();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUrl = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -16,10 +16,7 @@ function Auth0ProviderWithNavigate({ children }: Props) {
     }
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("User", user);
-        if (user?.sub && user?.email) {
-            createUser({ auth0Id: user.sub, email: user.email });
-        }
+        navigate("/auth-callback");
     };
 
     return (
