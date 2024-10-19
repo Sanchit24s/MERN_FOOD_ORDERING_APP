68f381f67d3cadd0117bdd0b15a0f4504f37a902
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-10-19 23:24:52 +0530

    Update my restaurant request

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,18 +1,22 @@
 import {
     useCreateMyRestaurant,
     useGetMyRestaurant,
+    useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 function ManageRestaurantPage() {
-    const { createRestaurant, isLoading } = useCreateMyRestaurant();
+    const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
     const { restaurant } = useGetMyRestaurant();
+    const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
+
+    const isEditing = !!restaurant;
 
     return (
         <ManageRestaurantForm
             restaurant={restaurant}
-            onSave={createRestaurant}
-            isLoading={isLoading}
+            onSave={isEditing ? updateRestaurant : createRestaurant}
+            isLoading={isCreateLoading || isUpdateLoading}
         />
     );
 }
