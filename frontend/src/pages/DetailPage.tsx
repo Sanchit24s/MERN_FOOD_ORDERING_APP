682aedc0105b0c2036edbe99171a34c567b0bae5
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-10-21 09:24:47 +0530

    Add to cart

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,12 +1,54 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
 import MenuItems from "@/components/MenuItems";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem as MenuItemType } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type CartItem = {
+    _id: string;
+    name: string;
+    price: number;
+    quantity: number;
+};
 
 function DetailPage() {
     const { restaurantId } = useParams();
     const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    const addToCard = (menuItem: MenuItemType) => {
+        setCartItems((prevCardItems) => {
+            const existingCartItem = prevCardItems.find(
+                (cartItem) => cartItem._id === menuItem._id
+            );
+            let updatedCartItems;
+
+            if (existingCartItem) {
+                updatedCartItems = prevCardItems.map((cartItem) =>
+                    cartItem._id === menuItem._id
+                        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                        : cartItem
+                );
+            } else {
+                updatedCartItems = [
+                    ...prevCardItems,
+                    {
+                        _id: menuItem._id,
+                        name: menuItem.name,
+                        price: menuItem.price,
+                        quantity: 1,
+                    },
+                ];
+            }
+
+            return updatedCartItems;
+        });
+    };
 
     if (isLoading || !restaurant) {
         return "Loading...";
@@ -25,8 +67,16 @@ function DetailPage() {
                     <RestaurantInfo restaurant={restaurant} />
                     <span className="text-2xl font-bold tracking-tight">Menu</span>
                     {restaurant.menuItems.map((menuItem) => (
-                        <MenuItems menuItem={menuItem} />
+                        <MenuItems
+                            menuItem={menuItem}
+                            addToCart={() => addToCard(menuItem)}
+                        />
                     ))}
+                </div>
+                <div>
+                    <Card>
+                        <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+                    </Card>
                 </div>
             </div>
         </div>
